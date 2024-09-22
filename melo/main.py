892384d0b02c9fdeaac28e841563f8bbd233a54f
uyplayer <uyplayer@outlook.com>
2024-09-22 16:39:15 +0800

    change ug  tokenized

--- melo/main.py
+++ melo/main.py
@@ -7,7 +7,7 @@ import os
 @click.argument('text')
 @click.argument('output_path')
 @click.option("--file", '-f', is_flag=True, show_default=True, default=False, help="Text is a file")
-@click.option('--language', '-l', default='EN', help='Language, defaults to English', type=click.Choice(['EN', 'ES', 'FR', 'ZH', 'JP', 'KR'], case_sensitive=False))
+@click.option('--language', '-l', default='EN', help='Language, defaults to English', type=click.Choice(['EN', 'ES', 'FR', 'ZH', 'JP', 'KR','UG'], case_sensitive=False))
 @click.option('--speaker', '-spk', default='EN-Default', help='Speaker ID, only for English, leave empty for default, ignored if not English. If English, defaults to "EN-Default"', type=click.Choice(['EN-Default', 'EN-US', 'EN-BR', 'EN_INDIA', 'EN-AU']))
 @click.option('--speed', '-s', default=1.0, help='Speed, defaults to 1.0', type=float)
 @click.option('--device', '-d', default='auto', help='Device, defaults to auto')
@@ -22,6 +22,8 @@ def main(text, file, output_path, language, speaker, speed, device):
         raise ValueError('You entered empty text or the file you passed was empty.')
     language = language.upper()
     if language == '': language = 'EN'
+    if language == 'EN':
+        speaker = ''
     if speaker == '': speaker = None
     if (not language == 'EN') and speaker:
         warnings.warn('You specified a speaker but the language is English.')
